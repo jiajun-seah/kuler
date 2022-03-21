d69dcc39201f0ad38786ce232107461814713d4b
Martin <martinchuang1999@gmail.com>
2022-03-21 15:37:21 +0800

    Rounded cart input corners

--- src/components/OrderDetails.vue
+++ src/components/OrderDetails.vue
@@ -23,7 +23,7 @@
     <div class="d-flex justify-content-center">
       
       <div class="form-group col-md-9">
-        <input type="text" id="name" name="email" /><br />
+        <input type="text" id="email" name="email" /><br />
       </div>
     </div>
     <br/>
@@ -58,7 +58,7 @@ p {
   color: red;
   font-weight: bold;
 }
-#name {
+#name, #email {
   width: 100%;
   background-color: #efefef;
   border: 3px solid #efefef;
@@ -66,9 +66,12 @@ p {
   border-right: none;
   padding: 5px;
   height: 40px;
-  border-radius: 20px 0 0 20px;
+  border-radius: 20px 20px 20px 20px;
   outline: none;
   color: #3a3d3b;
   font-family: "Open Sans", Verdana, Geneva, Tahoma, sans-serif;
+}
+.input {
+    border-radius: 20px 20px 20px 20px;
 }
 </style>
